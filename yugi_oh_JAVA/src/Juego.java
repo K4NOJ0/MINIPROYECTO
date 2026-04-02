0cc156dbad30fc8ba38e21e813e6a9d9361f59fa
josecuervo-desing <jose.cuervo@correounivalle.edu.co>
2026-04-02 11:18:13 -0500

    se realizaron los cambios de mecanica de turnos en la clase juego

--- yugi_oh_JAVA/src/Juego.java
+++ yugi_oh_JAVA/src/Juego.java
@@ -8,7 +8,7 @@ private boolean primerTurno = true;
 public void Menu(){
     System.out.println("====BIENVENIDO A YU-GI-OH====");
     System.out.println("QUE DESEA REALIZAR ");
-    System.out.println("1.INICIAR PARTDIDA ");
+    System.out.println("1.INICIAR PARTIDA ");
     System.out.println("2.SALIR");
     System.out.println("=============================");
 }
@@ -17,10 +17,11 @@ public void menuJuego(Jugador turno){
     System.out.println("=======================");
     System.out.println("JUEGA ["+turno.getNombre()+"]");
     System.out.println("QUE DESEA REALIZAR");
-    System.out.println("1.ATACAR AL ENEMIGO");
-    System.out.println("2.USAR CARTA MAGICA");
-    System.out.println("3.PASAR TURNO");
-    }
+    System.out.println("1. ATACAR AL ENEMIGO");
+    System.out.println("2. INVOCAR MONSTRUO");
+    System.out.println("3. USAR CARTA MAGICA");
+    System.out.println("4. PASAR TURNO");
+}
 
  public void mostrarInfoJugadores(Jugador j1,Jugador j2){
     System.out.println("===========================ESTADO DE LOS JUGADORES============================");
@@ -28,10 +29,27 @@ public void menuJuego(Jugador turno){
     System.out.println("PUNTOS DE LP ["+j1.getLp()+"]");
     j1.mostrarMano();
     System.out.println("monstruos en el campo: ");
+    if(j1.getCampo().getZonaMonstruos().isEmpty()){
+        System.out.println("EL JUGADOR NO TIENE MOSTRUOS EN EL CAMPO");
+    }else{
+    for (Monstruo m : j1.getCampo().getZonaMonstruos()) {
+    System.out.println("- " + m.getNombre() + " ATK: " + m.getAtk());
+    }
+   
+    }
     System.out.println("-------------------------------------------------------------------------------");
     System.out.println("JUGADOR ["+j2.getNombre()+"]");
     System.out.println("PUNTOS DE LP ["+j2.getLp()+"]");
     j2.mostrarMano();
+    System.out.println("monstruos en el campo: ");
+    if(j2.getCampo().getZonaMonstruos().isEmpty()){
+        System.out.println("EL JUGADOR NO TIENE MOSTRUOS EN EL CAMPO");
+    }else{
+        for (Monstruo m : j2.getCampo().getZonaMonstruos()) {
+        System.out.println("- " + m.getNombre() + " ATK: " + m.getAtk());
+    }
+    
+}
     System.out.println("============================================================================");
     
 }
@@ -76,6 +94,8 @@ public void iniciarJuego(Jugador j1, Jugador j2, Jugador turno){
         j2.robarCarta();
     }
 }
+
+
 public Jugador ejecutarTurno(Jugador actual, Jugador enemigo, Scanner escan) {
 
     System.out.println("\n=======================");
@@ -83,7 +103,7 @@ public Jugador ejecutarTurno(Jugador actual, Jugador enemigo, Scanner escan) {
 
     // 🔹 ROBAR CARTA
     if (actual.getMazo().isEmpty()) {
-        System.out.println(actual.getNombre() + " pierde por no tener cartas");
+        System.out.println(actual.getNombre() + " NO TIENE CARTAS EN EL MAZO");
         return null;
     }
     actual.robarCarta();
@@ -99,17 +119,22 @@ public Jugador ejecutarTurno(Jugador actual, Jugador enemigo, Scanner escan) {
 
             case 1: // ATACAR
                 if (primerTurno) {
-                    System.out.println("❌ No se puede atacar en el primer turno");
+                    System.out.println("NO SE PUEDE ATACAR EN EL PRIMER TURNO ESCOJA OTRA OPCION");
                 } else {
                     if (actual.getCampo().getZonaMonstruos().isEmpty()) {
-                        System.out.println("No tienes monstruos para atacar");
+                        System.out.println("NO TIENES MOSTRUOS EN EL CAMPO PARA ATACAR");
                     } else {
+                         System.out.println("TUS MONSTRUOS:");
+                        for (int i = 0; i < actual.getCampo().getZonaMonstruos().size(); i++) {
+                            Monstruo m = actual.getCampo().getZonaMonstruos().get(i);
+                            System.out.println(i + ". " + m.getNombre() + " ATK: " + m.getAtk());
+                        }
                         // ataque 
                         Monstruo atacante = actual.getCampo().getZonaMonstruos().get(0);
 
                         if (enemigo.getCampo().getZonaMonstruos().isEmpty()) {
                             enemigo.restarLP(atacante.getAtk());
-                            System.out.println("Ataque directo!");
+                            System.out.println("ATAQUE DIRECTO!");
                         } else {
                             Monstruo defensor = enemigo.getCampo().getZonaMonstruos().get(0);
                             actual.atacar(atacante, defensor, enemigo);
@@ -117,48 +142,96 @@ public Jugador ejecutarTurno(Jugador actual, Jugador enemigo, Scanner escan) {
                     }
                 }
                 break;
+                case 2: // INVOCAR MONSTRUO
 
-            case 2: // USAR CARTA
-                if (yaJugoCarta) {
-                    System.out.println("❌ Ya jugaste una carta este turno");
-                    break;
-                }
+    if (yaJugoCarta) {
+        System.out.println("YA INVOCASTE ESTE TURNO");
+        break;
+    }
 
-                if (actual.getMano().isEmpty()) {
-                    System.out.println("No tienes cartas");
-                    break;
-                }
+    // mostrar solo monstruos
+    System.out.println("ELIGE UN MONSTRUO:");
+    for (int i = 0; i < actual.getMano().size(); i++) {
+        if (actual.getMano().get(i) instanceof Monstruo) {
+            System.out.println(i + ". " + actual.getMano().get(i).getNombre());
+        }
+    }
 
-                Carta carta = actual.getMano().remove(0);
+    int eleccion = escan.nextInt();
 
-                if (carta instanceof Monstruo) {
-                    boolean invocado = actual.getCampo().invocarMonstruo((Monstruo) carta);
-                    if (invocado) {
-                        System.out.println("Invocaste: " + carta.getNombre());
-                        yaJugoCarta = true;
-                    } else {
-                        System.out.println("Campo lleno");
-                        actual.getMano().add(carta);
-                    }
+    if (eleccion < 0 || eleccion >= actual.getMano().size() ||
+        !(actual.getMano().get(eleccion) instanceof Monstruo)) {
+        System.out.println("SELECCION INVALIDA");
+        break;
+    }
 
-                } else if (carta instanceof CartaMagica) {
-                    ((CartaMagica) carta).activarEfecto(actual);
-                    yaJugoCarta = true;
-                }
+    Monstruo m = (Monstruo) actual.getMano().remove(eleccion);
 
-                break;
+    if (actual.getCampo().invocarMonstruo(m)) {
+        System.out.println("INVOCASTE: " + m.getNombre());
+        yaJugoCarta = true;
+    } else {
+        System.out.println("CAMPO LLENO");
+        actual.getMano().add(m);
+    }
 
-            case 3:
-                System.out.println("Fin del turno");
-                break;
+    break;
+
+           
+
+    
+        case 3: // USAR CARTA MAGICA
+
+    // mostrar solo mágicas
+    System.out.println("ELIGE UNA CARTA MAGICA:");
+    for (int i = 0; i < actual.getMano().size(); i++) {
+        if (actual.getMano().get(i) instanceof CartaMagica) {
+            System.out.println(i + ". " + actual.getMano().get(i).getNombre());
+        }
+    }
+
+    int eleccionMagica = escan.nextInt();
+
+    if (eleccionMagica < 0 || eleccionMagica >= actual.getMano().size() ||
+        !(actual.getMano().get(eleccionMagica) instanceof CartaMagica)) {
+        System.out.println("SELECCION INVALIDA");
+        break;
+    }
+
+    CartaMagica magia = (CartaMagica) actual.getMano().remove(eleccionMagica);
+    magia.activarEfecto(actual);
+
+    System.out.println("ACTIVASTE: " + magia.getNombre());
+
+    break;
+
+    case 4:
+    System.out.println("FIN DEL TURNO");
+    break;
+
 
             default:
-                System.out.println("Opción inválida");
+                System.out.println("OPCION INVALIDA");
         }
 
-    } while (opcion != 3);
+    } while (opcion != 4);
 
     primerTurno = false;
+
+    // 🔹 VERIFICAR SI ALGUIEN PERDIÓ POR LP
+if (enemigo.getLp() <= 0) {
+    System.out.println("=================================");
+    System.out.println("EL GANADOR ES: " + actual.getNombre());
+    System.out.println("=================================");
+    return null;
+}
+
+if (actual.getLp() <= 0) {
+    System.out.println("=================================");
+    System.out.println("EL GANADOR ES: " + enemigo.getNombre());
+    System.out.println("=================================");
+    return null;
+}
 
     //  CAMBIO DE TURNO
     return enemigo;
